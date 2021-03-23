5aa67ae610a9e352bcca22f341187c2c04162ca9
Rigoberto Contreras <rigoc1994@gmail.com>
2021-03-22 20:56:19 -0400

    a

--- login/index.js
+++ login/index.js
@@ -1,12 +1,15 @@
 const AWS =  require("aws-sdk")
+const bcrypt =  require("bcryptjs")
+const config =  require("./config.json")
+const dynamo = new AWS.DynamoDB();
+var cognitoidentity = new AWS.CognitoIdentity({region: 'us-east-1'});
 
 
-const dynamo = new AWS.DynamoDB().DocumentClient()
-
+//finds a user
 const findUser = async(email)=>{
     
  const params  = {
-       key:{
+       Key:{
            "Email":{"S":email}
        },
        TableName: "Users"
@@ -14,27 +17,106 @@ const findUser = async(email)=>{
 
  try {
        const result =  await dynamo.getItem(params).promise()
+       console.log("in Functino", result)
        return result
  } catch (error) {
+       console.log(error)
        return error
  }
 
 }
+////////////////////
+//validates user 
+const validateUser  = async(password,hashPassword)=>{
+    
+    try {
+         const isPassword = await bcrypt.compare(password,hashPassword)
+     
+     return isPassword;
+    } catch (e) {
+        
+    }
+    
+}
+////////
+
+const getID = async()=>{
+ 
+        let param = {
+		IdentityPoolId: config.IDENTITY_POOL_ID,
+		Logins: {} // To have provider name in a variable
+	};
+	
+param.Logins[config.DEVELOPER_PROVIDER_NAME] = config.DEVELOPER_PROVIDER_NAME
+const data = await cognitoidentity.getOpenIdTokenForDeveloperIdentity(param).promise()
+
+
+return data 
+
+    
+}
+
+
+/////////
+
+const buildResponse =  async(code,data)=>{
+   const response = {
+        'statusCode':code,
+        'headers': {
+            "Content-Type": "application/json"
+        },
+        'body':JSON.stringify(data)
+   }    
+   
+   return response
+}
+
+///////
+
 
 
 exports.handler = async(event,context) =>{
     let body =  JSON.parse(event.body)
-    
-    const user =  await  findUser(body.email)
+    let email =  String(body.Email)
+    let password =  String(body.Password)
+
+    try {
+    const user =  await findUser(email)
+    let hashPassword = String(user.Item.hashPassword.S)
+    const isUser =  await validateUser(password,hashPassword)
+    if(isUser){
+        
+      let tokens =  await getID()
+      
+      let response  = await buildResponse(200,tokens)
+      
+      
+      return response
+          
+       
    
-    const response = {
-         status:200
+    
+    }else{
+        
+     let response = {
+         "statusCode":404,
+         
+         "headers": {
+            "Content-Type": "application/json"
+        },
+         "body":JSON.stringify({msg:"User not found"})
     }
     
-    response.user = user
-  
-    return response;
-
+    return response
+        
+    }
+      } catch (e) {
+          
+          let response = {StatusCode:500,body:JSON.stringify(e)}  
+          
+          return response
+        
+      }
 
     
 }
